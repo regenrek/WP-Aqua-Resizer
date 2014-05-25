e905e9f9adf899746b645de6308bad6f171058e5
regenrek <kevinkernegger@kechbook.home>
2014-05-25 22:27:38 +0200

    wp_aqua_resizer plugin support

---
+++ wp_aqua_resizer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP Aqua Resizer
+ * Plugin URI:  https://github.com/kkern/wp-aqua-resizer
+ * Text Domain: wp-aqua-resizer
+ * Domain Path: /languages
+ * Description: Resize images on the fly - an TimThumb alternative with WordPress defaults
+ * Version:     1.0.0
+ * Author:      Kevin Regenrek, Syamil MJ
+ * Author URI:  http://regenrek.at
+ * License:     BSD
+ * 
+ * Aqua-Resizer
+ * https://github.com/syamilmj/Aqua-Resizer
+ * Creator - Syamil MJ
+ * 
+ */
+
+! defined( 'ABSPATH' ) and exit;
+require_once 'aq_resizer.php';
